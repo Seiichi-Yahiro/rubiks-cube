cccf24a26238fcd8c9d38b855cb786a64ce7fa5a
Julian Viechter <julian.viechter@ses.com>
2019-07-16 10:41:16 +0200

    Clean cube.tsx

--- src/tsx/cube/Cube.tsx
+++ src/tsx/cube/Cube.tsx
@@ -3,10 +3,10 @@ import { keys } from 'lodash';
 import { defaultColors, transitionedCubeClass } from './CubeUtils';
 import { Face, Layer, Layers } from './CubeTypes';
 import D3 from './D3';
-import Arrows from './Arrows';
 import Maybe from '../utils/Maybe';
 import Quaternion from 'quaternion';
 import { settingsContext } from '../context/SettingsContext';
+import Faces from './Faces';
 
 interface CubeProps {
     size: number;
@@ -15,7 +15,7 @@ interface CubeProps {
     rotationAnimation: Maybe<D3>;
     faceArrows: Layers<Maybe<[D3, D3]>>;
     rotate: (axis: D3) => void;
-    colors?: Partial<Layers<string>>;
+    colors: Partial<Layers<string>>;
 }
 
 const Cube: React.FunctionComponent<CubeProps> = ({
@@ -25,40 +25,31 @@ const Cube: React.FunctionComponent<CubeProps> = ({
     rotationAnimation,
     faceArrows,
     rotate,
-    colors: colorProps = defaultColors
+    colors
 }) => {
     const { rotationAnimationSpeed } = useContext(settingsContext);
 
-    const faces: Layers<Face> = useMemo(() => {
-        const colors: Layers<string> = {
-            ...defaultColors,
-            ...colorProps
-        };
-
+    const faceRotations = useMemo(() => {
         const halfCubeSize = size / 2;
 
-        const rotations: Layers<string> = {
+        return {
             [Layer.FRONT]: `translateZ(${halfCubeSize}px)`,
             [Layer.BACK]: `rotateY(180deg) translateZ(${halfCubeSize}px)`,
             [Layer.RIGHT]: `rotateY(90deg) translateZ(${halfCubeSize}px)`,
             [Layer.LEFT]: `rotateY(-90deg) translateZ(${halfCubeSize}px)`,
             [Layer.UP]: `rotateX(90deg) translateZ(${halfCubeSize}px)`,
             [Layer.DOWN]: `rotateX(-90deg) translateZ(${halfCubeSize}px)`
-        };
+        } as Layers<string>;
+    }, [size]);
 
-        return keys(rotations)
-            .map(
-                face =>
-                    ({
-                        [face]: {
-                            rotation: rotations[face],
-                            color: colors[face],
-                            arrowAxes: faceArrows[face]
-                        }
-                    } as Partial<Face>)
-            )
-            .reduce((a, b) => ({ ...a, ...b })) as Layers<Face>;
-    }, [colorProps, size, faceArrows]);
+    const faceColors: Layers<Face> = useMemo(
+        () =>
+            (({
+                ...defaultColors,
+                ...colors
+            } as unknown) as Layers<Face>),
+        [colors]
+    );
 
     const rotationMatrix3d = rotation.toMatrix4().map(Math.round);
     const rotate3d = rotationAnimation.let(it => it.toVector().join(',')).getOrElse('0,0,0');
@@ -74,62 +65,18 @@ const Cube: React.FunctionComponent<CubeProps> = ({
 
     return (
         <div className={transitionedCubeClass} style={cubeStyle}>
-            {keys(faces).map(face => {
-                const { rotation: faceRotation, color, arrowAxes } = faces[face] as Face;
-
-                return (
-                    <Faces
-                        key={faceRotation}
-                        size={size}
-                        color={color}
-                        rotation={faceRotation}
-                        rotate={rotate}
-                        arrowAxes={arrowAxes}
-                    />
-                );
-            })}
+            {keys(faceRotations).map(key => (
+                <Faces
+                    key={faceRotations[key]}
+                    size={size}
+                    color={faceColors[key]}
+                    rotation={faceRotations[key]}
+                    rotate={rotate}
+                    arrowAxes={faceArrows[key]}
+                />
+            ))}
         </div>
     );
 };
-
-interface FaceProps {
-    size: number;
-    color: string;
-    rotation: string;
-    rotate: (axis: D3) => void;
-    arrowAxes: Maybe<[D3, D3]>;
-}
-
-const Faces: React.FunctionComponent<FaceProps> = React.memo(({ size, color, rotation, rotate, arrowAxes }) => {
-    const faceStyle: React.CSSProperties = {
-        position: 'absolute',
-        width: size,
-        height: size,
-        borderRadius: 5,
-        border: '2px solid #333'
-    };
-
-    return (
-        <div
-            style={{
-                ...faceStyle,
-                backgroundColor: color,
-                transform: rotation
-            }}
-        >
-            {arrowAxes.letOrElse(
-                ([up, right]) => (
-                    <Arrows
-                        up={() => rotate(up)}
-                        down={() => rotate(up.invert())}
-                        right={() => rotate(right)}
-                        left={() => rotate(right.invert())}
-                    />
-                ),
-                undefined!
-            )}
-        </div>
-    );
-});
 
 export default React.memo(Cube);
---
+++ src/tsx/cube/Faces.tsx
@@ -0,0 +1,46 @@
+import D3 from './D3';
+import Maybe from '../utils/Maybe';
+import React from 'react';
+import Arrows from './Arrows';
+
+interface FacesProps {
+    size: number;
+    color: string;
+    rotation: string;
+    rotate: (axis: D3) => void;
+    arrowAxes: Maybe<[D3, D3]>;
+}
+
+const Faces: React.FunctionComponent<FacesProps> = ({ size, color, rotation, rotate, arrowAxes }) => {
+    const faceStyle: React.CSSProperties = {
+        position: 'absolute',
+        width: size,
+        height: size,
+        borderRadius: 5,
+        border: '2px solid #333'
+    };
+
+    return (
+        <div
+            style={{
+                ...faceStyle,
+                backgroundColor: color,
+                transform: rotation
+            }}
+        >
+            {arrowAxes.letOrElse(
+                ([up, right]) => (
+                    <Arrows
+                        up={() => rotate(up)}
+                        down={() => rotate(up.invert())}
+                        right={() => rotate(right)}
+                        left={() => rotate(right.invert())}
+                    />
+                ),
+                undefined!
+            )}
+        </div>
+    );
+};
+
+export default React.memo(Faces);
