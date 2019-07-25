01e00c200a3e572f5d73780b120abfe8055192e5
Julian Viechter <julian.viechter@ses.com>
2019-07-25 14:03:24 +0200

    Turn algorithms into a searchable list

--- src/tsx/interface/Algorithms.tsx
+++ src/tsx/interface/Algorithms.tsx
@@ -1,87 +1,98 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { List, ListItemText } from '@material-ui/core';
 import ListItem from '@material-ui/core/ListItem';
 import Look2CFOP from '../cube/algorithms/CFOP';
-import Category from './Category';
 import { IAlgorithm } from '../cube/algorithms/AlgorithmTypes';
 import Misc from '../cube/algorithms/Misc';
 import { algorithmPlayerContext, AlgorithmStatus } from '../context/AlgorithmPlayerContext';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import StartConfiguration from './StartConfiguration';
+import { take, last } from 'lodash';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
 
-const categories = [Look2CFOP, Misc];
+const flattenAlgorithms = (algorithm: IAlgorithm): IAlgorithm[] =>
+    [algorithm]
+        .concat(
+            algorithm.children
+                .map(child => (child.notation ? child : { ...child, name: `${algorithm.name} / ${child.name}` }))
+                .flatMap(flattenAlgorithms)
+        )
+        .filter((item, index, list) => index + 1 >= list.length || item.notation || list[index + 1].notation);
+
+const categories = [Look2CFOP, Misc]
+    .flatMap(flattenAlgorithms)
+    .map(item => ({ ...item, children: [] } as IAlgorithm))
+    .reduce(
+        (list, algorithm) => {
+            if (!algorithm.notation) {
+                return list.concat(algorithm);
+            }
+
+            const lastItem = last(list)!;
+            lastItem.children.push(algorithm as IAlgorithm);
+            const body = take(list, list.length - 1);
+            return body.concat(lastItem);
+        },
+        [] as IAlgorithm[]
+    ) as IAlgorithm[];
+
+const filterCategories = (searchValue: string) =>
+    categories
+        .map(group => {
+            if (group.name.toLocaleLowerCase().includes(searchValue)) {
+                return group;
+            }
+
+            const children = group.children.filter(child => child.name.toLocaleLowerCase().includes(searchValue));
+
+            if (children.length === 0) {
+                return (undefined as unknown) as IAlgorithm;
+            }
+
+            return {
+                ...group,
+                children: children
+            };
+        })
+        .filter(group => group);
 
 const Algorithms: React.FunctionComponent = () => {
-    const [openedMenu, setOpenedMenu] = useState('');
-    const onChildClick = useCallback((menu: string) => setOpenedMenu(prevMenu => (prevMenu === menu ? '' : menu)), []);
+    const { setAlgorithmPlayerState, status: playerStatus } = useContext(algorithmPlayerContext);
+    const [filteredCategories, setFilteredCategories] = useState(categories);
+    const filter = (event: React.ChangeEvent<HTMLInputElement>) =>
+        setFilteredCategories(filterCategories(event.target.value.toLocaleLowerCase()));
 
     return (
-        <List disablePadding={true} dense={true} className="interface-list">
-            {categories.map(category => (
-                <RecursiveChild
-                    key={category.name}
-                    onClick={onChildClick}
-                    isOpen={category.name === openedMenu}
-                    name={category.name}
-                    children={category.children}
-                    notation={category.notation}
-                    startConfiguration={category.startConfiguration}
-                    depth={1}
-                />
+        <List disablePadding={true} dense={true} className="interface-list interface-algorithm-list">
+            <ListSubheader className="interface-algorithm-list__filer">
+                <TextField label="Search" fullWidth={true} onChange={filter} />
+            </ListSubheader>
+            {filteredCategories.map((group, index) => (
+                <React.Fragment key={group.name + index}>
+                    <ListSubheader className="interface-list__item interface-algorithm-list__header">
+                        <Divider />
+                        {group.name}
+                        <Divider />
+                    </ListSubheader>
+                    {group.children.map(child => (
+                        <ListItem
+                            key={child.name + index}
+                            className="interface-list__item--moves"
+                            button={true}
+                            onClick={() => setAlgorithmPlayerState({ notation: child.notation })}
+                            disabled={playerStatus !== AlgorithmStatus.STOPPED}
+                        >
+                            {child.startConfiguration && (
+                                <StartConfiguration configuration={child.startConfiguration} />
+                            )}
+                            <ListItemText primary={child.name} secondary={child.notation} />
+                        </ListItem>
+                    ))}
+                </React.Fragment>
             ))}
         </List>
     );
 };
 
 export default Algorithms;
-
-interface IRecursiveChildProps extends IAlgorithm {
-    onClick: (menu: string) => void;
-    isOpen: boolean;
-    depth: number;
-}
-
-const RecursiveChild: React.FunctionComponent<IRecursiveChildProps> = React.memo(
-    ({ name, children, notation = '', startConfiguration, onClick, isOpen, depth }) => {
-        const { setAlgorithmPlayerState, status: playerStatus } = useContext(algorithmPlayerContext);
-        const [openedMenu, setOpenedMenu] = useState('');
-        const onChildClick = useCallback(
-            (menu: string) => setOpenedMenu(prevMenu => (prevMenu === menu ? '' : menu)),
-            []
-        );
-        const onSelfClick = useCallback(() => onClick(name), [name, onClick]);
-
-        if (children.length > 0) {
-            return (
-                <Category isOpen={isOpen} setMenu={onSelfClick} title={name}>
-                    <List disablePadding={true} dense={true} className="interface-list">
-                        {children.map(algorithm => (
-                            <RecursiveChild
-                                key={algorithm.name}
-                                isOpen={algorithm.name === openedMenu}
-                                onClick={onChildClick}
-                                name={algorithm.name}
-                                children={algorithm.children}
-                                notation={algorithm.notation}
-                                startConfiguration={algorithm.startConfiguration}
-                                depth={depth + 1}
-                            />
-                        ))}
-                    </List>
-                </Category>
-            );
-        } else {
-            const onAlgorithmClick = () => {
-                if (playerStatus === AlgorithmStatus.STOPPED) {
-                    setAlgorithmPlayerState({ notation });
-                }
-            };
-
-            return (
-                <ListItem className="interface-list__item--moves" button={true} onClick={onAlgorithmClick}>
-                    {startConfiguration && <StartConfiguration configuration={startConfiguration} />}
-                    <ListItemText primary={name} secondary={notation} />
-                </ListItem>
-            );
-        }
-    }
-);
--- src/tsx/interface/StartConfiguration.tsx
+++ src/tsx/interface/StartConfiguration.tsx
@@ -9,15 +9,18 @@ const StartConfiguration: React.FunctionComponent<IStartConfigurationProps> = ({
     <div className="start-configuration">
         {configuration.flatMap((row, rowIndex) =>
             row.map((color, colorIndex) => (
-                <div
-                    key={`${rowIndex}-${colorIndex}`}
-                    className="start-configuration__face"
-                    style={{
-                        backgroundColor: color,
-                        gridRow: rowIndex + 1,
-                        gridColumn: colorIndex + 1
-                    }}
-                />
+                <React.Fragment key={`${rowIndex}-${colorIndex}`}>
+                    {color !== CubeColors.TRANSPARENT && (
+                        <div
+                            className="start-configuration__face"
+                            style={{
+                                backgroundColor: color,
+                                gridRow: rowIndex + 1,
+                                gridColumn: colorIndex + 1
+                            }}
+                        />
+                    )}
+                </React.Fragment>
             ))
         )}
     </div>
