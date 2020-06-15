4cbb1169c8f9b0b10d8646969bdb4bc9d429c61f
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2020-06-15 09:07:54 +0200

    Update dependencies, switch to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -8,22 +9,29 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     entry: {
-        app: path.join(__dirname, 'src/index.tsx')
+        app: path.join(__dirname, 'src/index.tsx'),
     },
     output: {
         path: path.join(__dirname, 'build'),
         publicPath: '',
         filename: 'js/[name].js',
-        chunkFilename: 'js/[name].chunk.js'
+        chunkFilename: 'js/[name].chunk.js',
     },
     resolve: {
-        extensions: ['.js', '.ts', '.tsx']
+        extensions: ['.js', '.ts', '.tsx'],
     },
     module: {
         rules: [
             {
                 test: /\.(ts|tsx)$/,
-                loaders: ['awesome-typescript-loader']
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(s)?css$/,
@@ -32,41 +40,49 @@ module.exports = {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
                             sourceMap: true,
-                            publicPath: '../'
-                        }
+                            publicPath: '../',
+                        },
                     },
                     {
                         loader: 'css-loader',
                         options: {
-                            sourceMap: true
-                        }
+                            sourceMap: true,
+                        },
                     },
                     {
                         loader: 'sass-loader',
                         options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            }
-        ]
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
     },
     optimization: {
         splitChunks: {
             chunks: 'all',
-            automaticNameDelimiter: '.'
-        }
+            automaticNameDelimiter: '.',
+        },
     },
     plugins: [
+        new ForkTsCheckerWebpackPlugin({
+            eslint: {
+                files: './src/**/*',
+                options: {
+                    cache: true,
+                },
+            },
+        }),
         new HtmlWebpackPlugin({
             inject: 'head',
             filename: 'index.html',
-            template: 'src/index.html'
+            template: 'src/index.html',
         }),
         new MiniCssExtractPlugin({
             filename: 'css/[name].css',
-            chunkFilename: 'css/[name].chunk.css'
-        })
+            chunkFilename: 'css/[name].chunk.css',
+        }),
     ],
     devServer: {
         contentBase: 'build',
@@ -74,7 +90,7 @@ module.exports = {
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-        }
-    }
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+    },
 };
