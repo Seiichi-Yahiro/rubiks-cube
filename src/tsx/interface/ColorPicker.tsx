425ba2a0a929e70c3442cc4ee8e8df9a3406821d
Julian Viechter <jviechter@gmail.com>
2024-10-30 13:26:17 +0100

    Replace react-color with @ui/react-color

--- src/tsx/interface/ColorPicker.tsx
+++ src/tsx/interface/ColorPicker.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChromePicker } from 'react-color';
+import Chrome, { ChromeInputType } from '@uiw/react-color-chrome';
 import { useRedux } from '../states/States';
 import { Color } from '../cube/CubeTypes';
 import { useDispatch } from 'react-redux';
@@ -7,6 +7,8 @@ import { cubeActions } from '../states/cube/CubeActions';
 import useComplexState from '../hooks/useComplexState';
 import { Button, ClickAwayListener } from '@mui/material';
 import createClassName from '../utils/createClassName';
+import { GithubPlacement } from '@uiw/react-color-github';
+import './ColorPicker.css';
 
 interface State {
     selectedColor?: Color;
@@ -63,20 +65,21 @@ const ColorPicker: React.FunctionComponent = () => {
                     <div className="grid grid-cols-6 gap-1">{colors}</div>
                     {selectedColor && (
                         <div className="absolute z-10 mt-1">
-                            <ChromePicker
-                                disableAlpha={true}
+                            <Chrome
+                                className="hide-arrow" // special class to hide the GitHub placement arrow, as there doesn't seem to be a way to remove it programmatically
+                                showAlpha={false}
+                                placement={GithubPlacement.Top}
+                                inputType={ChromeInputType.HEXA}
                                 color={pickerColor}
-                                onChange={(color) =>
-                                    setState({ pickerColor: color.hex })
-                                }
-                                onChangeComplete={(color) =>
+                                onChange={(color) => {
+                                    setState({ pickerColor: color.hex });
                                     dispatch(
                                         cubeActions.setColor(
                                             selectedColor,
                                             color.hex,
                                         ),
-                                    )
-                                }
+                                    );
+                                }}
                             />
                         </div>
                     )}
