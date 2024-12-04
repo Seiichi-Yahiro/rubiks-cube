161cb0da52f07a88ff5507ce1812cfc26733b3ae
Julian Viechter <jviechter@gmail.com>
2024-12-04 19:14:34 +0100

    Add icon button

---
+++ src/tsx/components/IconButton.tsx
@@ -0,0 +1,58 @@
+import React, {
+    type ButtonHTMLAttributes,
+    type MouseEvent as ReactMouseEvent,
+    useCallback,
+    useMemo,
+} from 'react';
+import cn from 'src/utils/cn';
+
+const IconButton: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+    className,
+    children,
+    disabled,
+    onClick,
+    ...props
+}) => {
+    const click = useCallback(
+        (event: ReactMouseEvent<HTMLButtonElement, MouseEvent>) => {
+            if (!disabled) {
+                onClick?.(event);
+            }
+        },
+        [onClick, disabled],
+    );
+
+    const classNames = useMemo(
+        () =>
+            cn(
+                'relative inline-flex size-8 items-center justify-center p-1 outline-none *:fill-cube-gray *:stroke-cube-gray',
+
+                'before:absolute before:size-full before:rounded-full hover:before:bg-cube-gray/10 focus-visible:before:bg-cube-gray/10 active:before:bg-cube-gray/20 focus-visible:motion-safe:before:animate-breath',
+
+                'after:absolute after:size-full after:scale-110 after:rounded-full after:bg-transparent after:transition-all after:duration-[400ms] after:ease-out',
+
+                'active:after:scale-0 active:after:bg-cube-gray/50 active:after:transition-none',
+
+                {
+                    '*:opacity-50 hover:before:bg-transparent active:before:bg-transparent':
+                        disabled,
+                },
+
+                className,
+            ),
+        [disabled, className],
+    );
+
+    return (
+        <button
+            className={classNames}
+            aria-disabled={disabled}
+            onClick={click}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default React.memo(IconButton);
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,14 @@ export default {
                     '0%, 100%': { transform: 'rotate(-3deg)' },
                     '50%': { transform: 'rotate(3deg)' },
                 },
+                breath: {
+                    '0%, 100%': { transform: 'scale(1)' },
+                    '50%': { transform: 'scale(0.95)' },
+                },
             },
             animation: {
                 wiggle: 'wiggle 1s ease-in-out infinite',
+                breath: 'breath 1.5s ease-in-out infinite',
             },
             colors: {
                 white: '#ffffff',
