3e8bc245a66b23065cb48d4a4927baffa325f3e0
Julian Viechter <jviechter@gmail.com>
2024-12-07 20:26:13 +0100

    Give tooltip and popover the same light style

--- src/tsx/components/Tooltip.tsx
+++ src/tsx/components/Tooltip.tsx
@@ -16,7 +16,12 @@ const TooltipContent = React.forwardRef<
         ref={ref}
         sideOffset={sideOffset}
         className={cn(
-            'z-50 overflow-hidden rounded-md bg-cube-gray px-2 py-1 text-sm text-white opacity-95 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+            'z-50 overflow-hidden rounded-md border border-disabled bg-white px-2 py-1 text-sm text-cube-gray opacity-95 shadow-md',
+
+            'animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2',
+
+            'data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+
             className,
         )}
         {...props}
