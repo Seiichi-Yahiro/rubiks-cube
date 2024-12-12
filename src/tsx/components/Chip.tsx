a1946a8fe773e975b9c58ee3ec109164562cbffa
Julian Viechter <jviechter@gmail.com>
2024-12-12 17:25:52 +0100

    Add chip component

---
+++ src/tsx/components/Chip.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import cn from 'src/utils/cn';
+
+interface ChipProps {
+    className?: string;
+    children: React.ReactNode;
+    variant: 'primary' | 'error';
+}
+
+const Chip: React.FC<ChipProps> = ({ className, children, variant }) => (
+    <div
+        className={cn(
+            'inline-flex items-center rounded-full px-2.5 py-0.5 text-xs text-white',
+            {
+                'bg-cube-blue': variant === 'primary',
+                'bg-cube-red': variant === 'error',
+            },
+            className,
+        )}
+    >
+        {children}
+    </div>
+);
+
+export default Chip;
