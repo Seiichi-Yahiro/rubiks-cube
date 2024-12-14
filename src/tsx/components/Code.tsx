b238f07d79145c6918ad36a55554e94707a9ae2e
Julian Viechter <jviechter@gmail.com>
2024-12-14 16:09:32 +0100

    Add code component

---
+++ src/tsx/components/Code.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Code: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+    <span className="rounded bg-gray-200 p-0.5 font-mono text-cube-gray">
+        {children}
+    </span>
+);
+
+export default Code;
