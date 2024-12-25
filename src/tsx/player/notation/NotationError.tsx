1e1f21f6ad349183a21c0d9f57101f01e024e2a0
Julian Viechter <jviechter@gmail.com>
2024-12-25 14:09:04 +0100

    Use flex for notation errors

--- src/tsx/player/notation/NotationError.tsx
+++ src/tsx/player/notation/NotationError.tsx
@@ -1,13 +1,20 @@
 import { Failure } from 'parsimmon';
 import React from 'react';
 import Chip from 'src/tsx/components/Chip';
+import cn from 'src/utils/cn';
 
 interface NotationErrorProps {
     error: Failure;
+    className?: string;
 }
 
-const NotationError: React.FC<NotationErrorProps> = ({ error }) => (
-    <div className="my-1 space-x-0.5">
+const NotationError: React.FC<NotationErrorProps> = ({ error, className }) => (
+    <div
+        className={cn(
+            'flex flex-row flex-wrap items-center gap-0.5',
+            className,
+        )}
+    >
         {error.expected.map((errorMsg) => (
             <Chip key={errorMsg} variant="error">
                 {errorMsg}
